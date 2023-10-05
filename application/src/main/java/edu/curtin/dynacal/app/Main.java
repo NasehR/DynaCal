7cbb976d3d094ae6e9634c9a5f00543717279e2f
Naseh <75029758+NasehR@users.noreply.github.com>
2023-10-06 06:08:08 +0800

    Added the api subproject dependences to setting.gradle and build.gradle also including an example.

--- application/src/main/java/edu/curtin/dynacal/app/Main.java
+++ application/src/main/java/edu/curtin/dynacal/app/Main.java
@@ -1,7 +1,14 @@
 package edu.curtin.dynacal.app;
 
+import edu.curtin.dynacal.api.Api;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! from app");
+
+
+        Api api = new Api();
+
+        api.print();
     }
 }
