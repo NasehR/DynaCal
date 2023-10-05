9ef765c4a6a4dca9748954deae53fc882144fdae
Naseh <75029758+NasehR@users.noreply.github.com>
2023-10-06 07:30:27 +0800

    Added the subproject as a dependency to the application subproject.

--- application/src/main/java/edu/curtin/dynacal/app/Main.java
+++ application/src/main/java/edu/curtin/dynacal/app/Main.java
@@ -1,14 +1,16 @@
 package edu.curtin.dynacal.app;
 
 import edu.curtin.dynacal.api.Api;
-
+import edu.curtin.dynacal.calplugins.Repeat;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! from app");
 
 
         Api api = new Api();
+        Repeat repeat = new Repeat();
 
         api.print();
+        repeat.print();
     }
 }
