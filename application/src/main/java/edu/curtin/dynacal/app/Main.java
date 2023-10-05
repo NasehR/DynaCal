e136832571fd8298ebbd270bf4a86705aedd45c0
Naseh <75029758+NasehR@users.noreply.github.com>
2023-10-06 07:49:36 +0800

    Sample code.

--- application/src/main/java/edu/curtin/dynacal/app/Main.java
+++ application/src/main/java/edu/curtin/dynacal/app/Main.java
@@ -2,6 +2,7 @@ package edu.curtin.dynacal.app;
 
 import edu.curtin.dynacal.api.Api;
 import edu.curtin.dynacal.calplugins.Repeat;
+import edu.curtin.dynacal.calplugins.Notify;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! from app");
@@ -9,8 +10,10 @@ public class Main {
 
         Api api = new Api();
         Repeat repeat = new Repeat();
+        Notify notify = new Notify();
 
         api.print();
         repeat.print();
+        notify.print();
     }
 }
