61a03b47c7ba2977595db81478da3b50e0269ecd
Naseh <75029758+NasehR@users.noreply.github.com>
2023-10-06 07:29:21 +0800

    Created a subproject called 'calplugins-repeat'.

---
+++ calplugins-repeat/src/main/java/edu/curtin/dynacal/calplugins/Repeat.java
@@ -0,0 +1,13 @@
+package edu.curtin.dynacal.calplugins;
+
+import edu.curtin.dynacal.api.Api;
+
+public class Repeat {
+    public Repeat() {
+        System.out.println("Repeat Constructor");
+    }
+
+    public void print() {
+        System.out.println("Hello world! from repeat");
+    }
+}
