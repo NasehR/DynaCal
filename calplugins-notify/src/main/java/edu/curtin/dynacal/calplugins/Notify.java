1b155009f8d2125d09fb685c5f2f0eb34daa924e
Naseh <75029758+NasehR@users.noreply.github.com>
2023-10-06 07:49:08 +0800

    Created a subproject called 'calplugins-notify' and added it dependencies.

---
+++ calplugins-notify/src/main/java/edu/curtin/dynacal/calplugins/Notify.java
@@ -0,0 +1,13 @@
+package edu.curtin.dynacal.calplugins;
+
+import edu.curtin.dynacal.api.Api;
+
+public class Notify {
+    public Notify() {
+        System.out.println("Notify Constructor");
+    }
+
+    public void print() {
+        System.out.println("Hello world! from notify");
+    }
+}
