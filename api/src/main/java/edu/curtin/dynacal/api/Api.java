bb4fa18f0c4d85189bf90cb4196d2afdf63f442a
Naseh <75029758+NasehR@users.noreply.github.com>
2023-10-06 06:06:35 +0800

    Created a subproject called 'api'.

---
+++ api/src/main/java/edu/curtin/dynacal/api/Api.java
@@ -0,0 +1,11 @@
+package edu.curtin.dynacal.api;
+
+public class Api {
+    public Api() {
+        System.out.println("Api constructor");
+    }
+
+    public void print() {
+        System.out.println("Hello world! from api");
+    }
+}
