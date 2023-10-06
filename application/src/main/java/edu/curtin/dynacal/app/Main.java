99aed32aa96ae898d94747fd6cb1a97e1705673f
Naseh Rizvi <75029758+NasehR@users.noreply.github.com>
2023-10-06 13:16:46 +0800

    Created a calendar file with a template. Also wrote a sample code to read the calendar file in the main method.

--- application/src/main/java/edu/curtin/dynacal/app/Main.java
+++ application/src/main/java/edu/curtin/dynacal/app/Main.java
@@ -3,6 +3,11 @@ package edu.curtin.dynacal.app;
 import edu.curtin.dynacal.api.Api;
 import edu.curtin.dynacal.calplugins.Repeat;
 import edu.curtin.dynacal.calplugins.Notify;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! from app");
@@ -18,6 +23,22 @@ public class Main {
 
         for (String arg: args) {
             System.out.println(arg);
+        }
+
+
+
+
+        try {
+            File myObj = new File(args[0]);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
     }
 }
