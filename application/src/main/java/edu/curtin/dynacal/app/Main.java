b65aca40df1f817bc2014115de95e945b6acd2d2
Naseh Rizvi <75029758+NasehR@users.noreply.github.com>
2023-10-19 11:30:50 +0800

    Demo.

--- application/src/main/java/edu/curtin/dynacal/app/Main.java
+++ application/src/main/java/edu/curtin/dynacal/app/Main.java
@@ -3,9 +3,12 @@ package edu.curtin.dynacal.app;
 import edu.curtin.dynacal.api.Api;
 import edu.curtin.dynacal.calplugins.Repeat;
 import edu.curtin.dynacal.calplugins.Notify;
+import edu.curtin.terminalgrid.TerminalGrid;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -40,5 +43,91 @@ public class Main {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+
+
+
+
+        if (args.length > 0) {
+            for (String arg: args) {
+                System.out.println(arg);
+            }
+
+//            try {
+//                File myObj = new File(args[0]);
+//                Scanner myReader = new Scanner(myObj);
+//                while (myReader.hasNextLine()) {
+//                    String data = myReader.nextLine();
+//                    System.out.println(data);
+//                }
+//                myReader.close();
+//            } catch (FileNotFoundException e) {
+//                System.out.println("An error occurred.");
+//                e.printStackTrace();
+//            }
+        }
+
+
+        // Demonstration data
+        String[][] messages = {{"one two three",     "four five six",             "seven eight nine"},
+                {"ten eleven twelve", "thirteen fourteen fifteen", "sixteen seventeen eighteen"}};
+
+        String[] rowHeadings = {"row 1", "row 2"};
+        String[] colHeadings = {"column A", "column B", "column C"};
+
+
+        // Initialising
+        var terminalGrid = TerminalGrid.create();
+
+
+        // Without headings
+        terminalGrid.print(messages);
+        System.out.println();
+
+
+        // With headings
+        terminalGrid.print(messages, rowHeadings, colHeadings);
+        System.out.println();
+
+
+        // Using nested lists rather than arrays
+        var listMessages = new ArrayList<List<String>>();
+        var row1 = new ArrayList<String>();
+        var row2 = new ArrayList<String>();
+        row1.add("one");
+        row1.add("two");
+        row2.add("three");
+        row2.add("four");
+        listMessages.add(row1);
+        listMessages.add(row2);
+        terminalGrid.print(listMessages, List.of("row 1", "row 2"), List.of("col A", "col B"));
+        System.out.println();
+
+
+        // With limited space
+        terminalGrid.setTerminalWidth(42);
+        terminalGrid.print(messages, rowHeadings, colHeadings);
+        System.out.println();
+        terminalGrid.setTerminalWidth(120);
+
+
+        // Specifying UTF-8 character encoding explicitly (may help make box-drawing characters work)
+        terminalGrid.setCharset(java.nio.charset.Charset.forName("UTF-8"));
+        terminalGrid.print(messages, rowHeadings, colHeadings);
+        System.out.println();
+
+
+        // With plain ASCII characters (fallback if the real box-drawing characters just don't display properly)
+        terminalGrid.setBoxChars(TerminalGrid.ASCII_BOX_CHARS);
+        terminalGrid.print(messages, rowHeadings, colHeadings);
+        System.out.println();
+
+
+        // With custom box-drawing characters (if you must!)
+        terminalGrid.setBoxChars(new TerminalGrid.BoxChars(
+                "\u2502 ", " \u250a ", " \u2502",
+                "\u2500", "\u254c", "\u2500",
+                "\u256d\u2500", "\u2500\u256e", "\u2570\u2500", "\u2500\u256f",
+                "\u2500\u252c\u2500", "\u2500\u2534\u2500", "\u251c\u254c", "\u254c\u2524", "\u254c\u253c\u254c"));
+        terminalGrid.print(messages, rowHeadings, colHeadings);
     }
 }
