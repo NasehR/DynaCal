3373f948a0aa6c980486820dd621a5bc1d7cfe3b
Naseh Rizvi <75029758+NasehR@users.noreply.github.com>
2023-11-16 12:56:09 +0800

    Populated the calendar grid.

--- application/src/main/java/edu/curtin/dynacal/app/Main.java
+++ application/src/main/java/edu/curtin/dynacal/app/Main.java
@@ -1,11 +1,15 @@
 package edu.curtin.dynacal.app;
 
 import edu.curtin.dynacal.api.IEvent;
+import edu.curtin.dynacal.app.view.TerminalView;
 import edu.curtin.terminalgrid.TerminalGrid;
 import edu.curtin.dynacal.dsl.CalendarParser;
 import edu.curtin.dynacal.dsl.ParseException;
+import org.w3c.dom.events.Event;
+
 import java.io.IOException;
 import java.lang.reflect.Array;
+import java.security.cert.CertificateNotYetValidException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
@@ -47,12 +51,15 @@ public class Main {
         var terminalGrid = TerminalGrid.create();
         // TODO: Changed later for I18N
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM");
+        List<IEvent> eventList = List.of();
+        Map<String, Map<String, String>> plugInInfo;
+        List<String> scripts;
 
         try {
             p = CalendarParser.parse(args[0]);
-            List<IEvent> eventList = p.getEventList();
-            Map<String, Map<String, String>> plugInInfo = p.getPlugInInfo();
-            List<String> scripts = p.getScripts();
+            eventList = p.getEventList();
+            plugInInfo = p.getPlugInInfo();
+            scripts = p.getScripts();
 
 //            for ( IEvent event: eventList ) {
 //                System.out.println(event.getName());
@@ -70,55 +77,14 @@ public class Main {
 //                System.out.println(script);
 //            }
 
-        }
-        catch (ParseException exception) {
+        } catch (ParseException exception) {
+            System.out.println(exception.toString());
+        } catch (IOException exception) {
             System.out.println(exception.toString());
         }
-        catch (IOException exception) {
-            System.out.println(exception.toString());
-        }
 
+        TerminalView terminalView = new TerminalView(terminalGrid, eventList, dateFormatter);
+        terminalView.print();
 
-        // Demonstration data
-        String[][] events =  new String[25][7];
-        String[] times = new String[25];
-        String[] days = new String[7];
-
-        // events
-        for (int i = 0; i < 25; i++) {
-            for (int j = 0; j < 7; j++) {
-                events[i][j] = "";
-            }
-        }
-
-        //time
-        for (int i = 0; i < 25; i++) {
-            String time = "";
-            if (i == 0) {
-                time = "All-Day";
-            }
-            else if (i-1 < 10){
-                time = "0" + (i-1) + "00";
-            }
-            else {
-                time = (i-1) + "00";
-            }
-            times[i] = time;
-        }
-
-        //day
-        for (int i = 0; i < 7; i++) {
-            days[i] = LocalDate.now()
-                    .plusDays(i)
-                    .format(dateFormatter);
-        }
-
-        // With custom box-drawing characters (if you must!)
-        terminalGrid.setBoxChars(new TerminalGrid.BoxChars(
-                "\u2502 ", " \u250a ", " \u2502",
-                "\u2500", "\u254c", "\u2500",
-                "\u256d\u2500", "\u2500\u256e", "\u2570\u2500", "\u2500\u256f",
-                "\u2500\u252c\u2500", "\u2500\u2534\u2500", "\u251c\u254c", "\u254c\u2524", "\u254c\u253c\u254c"));
-        terminalGrid.print(events, times, days);
     }
 }
--- application/src/main/java/edu/curtin/dynacal/app/view/TerminalView.java
+++ application/src/main/java/edu/curtin/dynacal/app/view/TerminalView.java
@@ -1,4 +1,86 @@
 package edu.curtin.dynacal.app.view;
 
+import edu.curtin.dynacal.api.IEvent;
+import edu.curtin.terminalgrid.TerminalGrid;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
 public class TerminalView {
+
+    private TerminalGrid calendarGrid;
+    private List<IEvent> eventList;
+    DateTimeFormatter dateFormatter;
+
+    public TerminalView(TerminalGrid calendarGrid, List<IEvent> eventList, DateTimeFormatter dateFormatter) {
+        this.calendarGrid = calendarGrid;
+        this.eventList = eventList;
+        this.dateFormatter = dateFormatter;
+
+    }
+
+    public void print() {
+        // Demonstration data
+        String[][] events = new String[25][7];
+        String[] times = new String[25];
+        String[] days = new String[7];
+
+        // events
+        for (int i = 0; i < 25; i++) {
+            for (int j = 0; j < 7; j++) {
+                events[i][j] = "";
+            }
+        }
+
+        //time
+        for (int i = 0; i < 25; i++) {
+            String time = "";
+            if (i == 0) {
+                time = "All-Day";
+            } else if (i - 1 < 10) {
+                time = "0" + (i - 1) + "00";
+            } else {
+                time = (i - 1) + "00";
+            }
+            times[i] = time;
+        }
+
+        //day
+        for (int i = 0; i < 7; i++) {
+            days[i] = LocalDate.now()
+                    .plusDays(i)
+                    .format(dateFormatter);
+        }
+
+        for ( IEvent event : eventList) {
+            LocalDate startDate = event.getStartDate();
+            LocalDate startWeekDate = LocalDate.now();
+            LocalDate endWeekDate = LocalDate.now().plusDays(6);
+
+            if (startDate.isAfter(startWeekDate.minusDays(1)) && startDate.isBefore(endWeekDate.plusDays(1))) {
+                System.out.println("Start Date:\t" + event.getStartDate());
+
+                int dayIndex = startWeekDate.until(startDate).getDays();
+                int timeIndex = 0;
+
+                if (event.getStartTime().isPresent()) {
+                    timeIndex = event
+                            .getStartTime()
+                            .get()
+                            .getHour() + 1;
+                }
+
+                events[timeIndex][dayIndex] = events[timeIndex][dayIndex] + event.getName() + "\n";
+            }
+        }
+
+        // With custom box-drawing characters (if you must!)
+        calendarGrid.setBoxChars(new TerminalGrid.BoxChars(
+                "\u2502 ", " \u250a ", " \u2502",
+                "\u2500", "\u254c", "\u2500",
+                "\u256d\u2500", "\u2500\u256e", "\u2570\u2500", "\u2500\u256f",
+                "\u2500\u252c\u2500", "\u2500\u2534\u2500", "\u251c\u254c", "\u254c\u2524", "\u254c\u253c\u254c"));
+        calendarGrid.print(events, times, days);
+    }
 }
