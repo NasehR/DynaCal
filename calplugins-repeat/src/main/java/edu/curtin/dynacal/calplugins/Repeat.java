8f6f1ae9ed237d3ea94ac5801b6393eb7888b35c
Naseh <75029758+NasehR@users.noreply.github.com>
2023-11-16 08:17:53 +0800

    Implements the interface methods.

--- calplugins-repeat/src/main/java/edu/curtin/dynacal/calplugins/Repeat.java
+++ calplugins-repeat/src/main/java/edu/curtin/dynacal/calplugins/Repeat.java
@@ -1,5 +1,8 @@
 package edu.curtin.dynacal.calplugins;
 
+import edu.curtin.dynacal.api.API;
+import edu.curtin.dynacal.api.ICalendarPlugin;
+
 public class Repeat implements ICalendarPlugin {
 
     private API api;
