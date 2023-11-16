c84b8e82903a8b05a00b871111eeeb60191e7e5b
Naseh Rizvi <75029758+NasehR@users.noreply.github.com>
2023-11-16 14:58:37 +0800

    Initialisation of the plugins and added the event list into the calendar.

--- application/src/main/java/edu/curtin/dynacal/app/controller/CalendarController.java
+++ application/src/main/java/edu/curtin/dynacal/app/controller/CalendarController.java
@@ -1,4 +1,18 @@
 package edu.curtin.dynacal.app.controller;
 
-public class CalendarController {
+import edu.curtin.dynacal.api.API;
+import edu.curtin.dynacal.api.IEvent;
+import java.util.List;
+
+public class CalendarController implements API {
+    private List<IEvent> eventsList;
+
+    public CalendarController(List<IEvent> eventsList) {
+        this.eventsList = eventsList;
+    }
+
+    @Override
+    public void registerEvent(IEvent event) {
+        eventsList.add(event);
+    }
 }
--- application/src/main/java/edu/curtin/dynacal/app/controller/ExtraController.java
+++ application/src/main/java/edu/curtin/dynacal/app/controller/ExtraController.java
@@ -1,4 +1,37 @@
 package edu.curtin.dynacal.app.controller;
 
+import edu.curtin.dynacal.api.API;
+import edu.curtin.dynacal.api.ICalendarPlugin;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class ExtraController {
+
+    private List<ICalendarPlugin> calendarPluginList;
+    private API api;
+    public ExtraController(API api) {
+        calendarPluginList = new ArrayList<>();
+        this.api = api;
+    }
+
+    public void initaliser(Map<String, Map<String, String>> plugInInfo) {
+        for (var plugInEntry : plugInInfo.entrySet()) {
+            try {
+                Class<?> classTemp = Class.forName(plugInEntry.getKey());
+
+                if (ICalendarPlugin.class.isAssignableFrom(classTemp)) {
+                    ICalendarPlugin plugin = (ICalendarPlugin) classTemp.getConstructor().newInstance();
+                    calendarPluginList.add(plugin);
+
+                    plugin.start(api, plugInEntry.getValue());
+                }
+            }
+            catch (ClassNotFoundException | InvocationTargetException | InstantiationException |
+                     IllegalAccessException | NoSuchMethodException exception) {
+                System.out.println(exception.toString());
+            }
+        }
+    }
 }
